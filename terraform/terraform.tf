1eb5f2acde20601244dd61232b2364e8928e089a
ww24 <ww24@users.noreply.github.com>
2021-05-12 23:04:28 +0900

    fix: update terraform google provider

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "~> 0.15.1"
+  required_version = "~> 0.15.3"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 3.66.1"
+      version = "~> 3.67.0"
     }
   }
 
